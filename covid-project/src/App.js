c4b43c6f8c16f3f0a340703ada60076496e9783a
Btissam Yaqine <btissamyaqine123@gmail.com>
2021-01-17 16:27:24 +0100

    update covid-project

--- covid-project/src/App.js
+++ covid-project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   MenuItem,
   FormControl,
@@ -7,21 +7,50 @@ import {
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState(["use","uk","india"]);
+
+  // STATE = how to write a variable in React 
+
+  // USEEFFECT runs a peice of code 
+  // based on a given condition 
+
+  useEffect(() => {
+    // async -> send a request, wait for it, do somthing with info
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/jhucsse/counties")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name:country.country, 
+            value: country.countryInfo.iso2
+          }
+        ))
+      })
+    }
+  }, [])
   return (
     //BEM naming convention 
     <div className="app"> 
     <div className="app__header">
-    <h1>COVID 19 TRACKER</h1>
+    <h1>COVID-19 TRACKER</h1>
       <FormControl className="app__dropdown">
         <Select
           variant="outlined"
           value="abc"
         >
+          {/* Loop through all the countries and show a drop down list of the options */}
+          {
+            countries.map(country => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+          }
 
-          <MenuItem value="worldwide">WorldWide</MenuItem>
+          {/* <MenuItem value="worldwide">WorldWide</MenuItem>
           <MenuItem value="worldwide">Option two</MenuItem>
           <MenuItem value="worldwide">Option tree</MenuItem>
-          <MenuItem value="worldwide">Option four</MenuItem>
+          <MenuItem value="worldwide">Option four</MenuItem> */}
         </Select>
       </FormControl>
     </div>
